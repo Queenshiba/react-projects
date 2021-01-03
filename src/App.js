344d7d8900578d2a8b1700f608226e5930d7df4c
Queenshiba <haruka0111.2a@gmail.com>
2021-01-03 21:03:24 +0900

    add input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>To Do List</h1>
 
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      value: ''
+    };
 
-      <body>
+  }
+  submitValue(params) {
+
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+  addTodo(e) {
+    this.setState({
+      todos: [...this.state.todos, this.state.value],
+      value: ''
+    })
+  e.preventDefault()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>To Do List</h1>
+
+        </header>
+
+
         <div className="input-container">
-          <form>
-            <input type="text" name="name" />
-            <input type="submit" value="Submit" />
+          <form onSubmit={(e) => this.addTodo(e)}>
+            <input className="input" placeholder="Type something..." type="text" name="name" value={this.state.value} onChange={(e) => this.handleChange(e)} />
+            <input className="add-btn" type="submit" value="Add" />
           </form>
+{
+  this.state.todos.map((todo, index) => {
+    return (<div key={todo+index}>{todo}</div>)
+  })
+}
+
+
+          <div className="todolist">list here</div>
         </div>
-      </body>
-    </div>
-  );
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
